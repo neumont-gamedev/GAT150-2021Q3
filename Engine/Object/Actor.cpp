7eac80be8e38faf976aff1af55cb62904b5d0b38
neumont-gamedev <rmaple@neumont.edu>
2021-08-06 14:07:34 -0600

    class renderer actor with texture

--- Engine/Object/Actor.cpp
+++ Engine/Object/Actor.cpp
@@ -1,21 +1,25 @@
 #include "Actor.h"
+#include "Graphics/Renderer.h"
 #include <algorithm>
 
 namespace nc
 {
 	void Actor::Update(float dt)
 	{
+		transform.rotation += 0.2f;
+
 		transform.Update();
 		std::for_each(children.begin(), children.end(), [](auto& child) { child->transform.Update(child->parent->transform.matrix);  });
 	}
 
-	void Actor::Draw()
+	void Actor::Draw(Renderer* renderer)
 	{
+		renderer->Draw(texture, transform);
 	}
 
 	float Actor::GetRadius()
 	{
-		return 0;
+		return std::max(texture->GetSize().x, texture->GetSize().y) * 0.5f;
 	}
 
 	void Actor::AddChild(std::unique_ptr<Actor> actor)
--- Game/Main.cpp
+++ Game/Main.cpp
@@ -7,15 +7,21 @@ int main(int, char**)
 {
 	nc::Engine engine;
 	engine.Startup();
-
 	engine.Get<nc::Renderer>()->Create("GAT150", 800, 600);
 
-	std::cout << nc::GetFilePath() << std::endl;
+	nc::Scene scene;
+	scene.engine = &engine;
+
 	nc::SetFilePath("../Resources");
-	std::cout << nc::GetFilePath() << std::endl;
 
 	std::shared_ptr<nc::Texture> texture = engine.Get<nc::ResourceSystem>()->Get<nc::Texture>("sf2.png", engine.Get<nc::Renderer>());
 
+	for (size_t i = 0; i < 10; i++)
+	{
+		nc::Transform transform{ nc::Vector2{nc::RandomRange(0, 800), nc::RandomRange(0, 600)}, nc::RandomRange(0, 360), 1.0f };
+		std::unique_ptr<nc::Actor> actor = std::make_unique<nc::Actor>(transform, texture);
+		scene.AddActor(std::move(actor));
+	}
 
 
 	bool quit = false;
@@ -30,25 +36,15 @@ int main(int, char**)
 			break;
 		}
 		
+		engine.Update(0);
+		scene.Update(0);
+
 		engine.Get<nc::Renderer>()->BeginFrame();
 
-		nc::Vector2 position{ 300, 400 };
-		engine.Get<nc::Renderer>()->Draw(texture, position);
+		scene.Draw(engine.Get<nc::Renderer>());
 
 		engine.Get<nc::Renderer>()->EndFrame();
-
-
-		//for (size_t i = 0; i < 50; i++)
-		//{
-		//	SDL_Rect src{ 32, 64, 32, 64 };
-		//	SDL_Rect dest{ nc::RandomRangeInt(0, screen.x), nc::RandomRangeInt(0, screen.y), 16, 24 };
-
-		//	SDL_RenderCopy(renderer, texture, &src, &dest);
-		//}
-
-		
 	}
-
 	
 	SDL_Quit();
 
